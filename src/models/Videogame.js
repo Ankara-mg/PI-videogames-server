f73acadba0366847422631df1478f7ff3226e4d4
Ankara.mg <solroman44@gmail.com>
2022-08-02 13:17:21 -0300

    models

--- src/models/Videogame.js
+++ src/models/Videogame.js
@@ -1,12 +1,32 @@
-const { DataTypes } = require('sequelize');
+const { DataTypes, UUIDV1 } = require('sequelize');
 // Exportamos una funcion que define el modelo
 // Luego le injectamos la conexion a sequelize.
 module.exports = (sequelize) => {
   // defino el modelo
   sequelize.define('videogame', {
+    id: {
+      type: DataTypes.UUID,
+      defaultValue: UUIDV1,
+      allowNull: false,
+      primaryKey: true
+    },
     name: {
       type: DataTypes.STRING,
       allowNull: false,
     },
+    description: {
+      type: DataTypes.TEXT,
+      allowNull: false,
+    },
+    releaseDate: {
+      type: DataTypes.DATEONLY
+    },
+    rating: {
+      type: DataTypes.FLOAT,
+    },
+    platforms: {
+      type: DataTypes.ARRAY(DataTypes.STRING),
+      allowNull: false,
+    }
   });
 };
--- src/routes/index.js
+++ src/routes/index.js
@@ -2,12 +2,16 @@ const { Router } = require('express');
 // Importar todos los routers;
 // Ejemplo: const authRouter = require('./auth.js');
 
-const apiKey = 'xxxxxxxx'
+require('dotenv').config();
+const { API_KEY } = process.env;
 
 const router = Router();
 
 // Configurar los routers
 // Ejemplo: router.use('/auth', authRouter);
 
+router.get('/videogames', async(req, res) => {
+    
+})
 
 module.exports = router;

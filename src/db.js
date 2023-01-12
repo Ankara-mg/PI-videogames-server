9fdc38084d39f664174b40d3b6f71ee5e64ec479
Sol <98844198+Ankara-mg@users.noreply.github.com>
2023-01-11 23:31:25 -0300

    Update db.js

--- src/db.js
+++ src/db.js
@@ -3,12 +3,12 @@ const { Sequelize } = require('sequelize');
 const fs = require('fs');
 const path = require('path');
 const {
-  PGUSER, PGPASSWORD, PGHOST, PGDATABASE
+  PGUSER, PGPASSWORD, PGHOST, PGDATABASE, PGPORT
 } = process.env;
 
 console.log(PGUSER, PGPASSWORD, PGHOST, PGDATABASE)
 // postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/videogames
-const sequelize = new Sequelize(`postgres://${PGUSER}:${PGPASSWORD}@${PGHOST}/${PGDATABASE}`, {
+const sequelize = new Sequelize(`postgres://${PGUSER}:${PGPASSWORD}@${PGHOST}:${PGPORT}/${PGDATABASE}`, {
   logging: false, // set to console.log to see the raw SQL queries
   native: false, // lets Sequelize know we can use pg-native for ~30% more speed
 });
